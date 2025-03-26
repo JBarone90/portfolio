14e978a7cdfee78b8220bf18b79cb8a55c69b5dc
Jacopo <baronej1990@gmail.com>
2025-03-26 14:05:45 +0000

    feat: Add Main

---
+++ src/components/Main.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<main class="flex flex-column pa4 mt5">
+  <section
+    id="IntroPage"
+    class="flex flex-column items-center lg:items-start gap-6 md:gap-8 lg:gap-10"
+  >
+    <div class="flex flex-column lg:justify-center text-center lg:text-left">
+      <h2 class="f3 f2-ns f1-l fw6">
+        Hi! I'm <span class="poppins purple">Jacopo</span> Barone <br />
+        Data <span class="poppins purple">Scientist</span>
+      </h2>
+      <p class="f6 f5-ns f4-m">
+        My <span class="purple"> favorite tech</span> includes JavaScript (NEXT.JS
+        or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL or Firebase/Firestore!
+      </p>
+    </div>
+  </section>
+</main>
