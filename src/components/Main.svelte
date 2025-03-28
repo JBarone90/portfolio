64c6b3236d97c0f00de9ee66e696dd453f45c806
Jacopo <baronej1990@gmail.com>
2025-03-28 00:12:14 +0000

    feat: add rolling text

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,4 +1,24 @@
 <script>
+  import { gsap } from "gsap";
+  import { onMount } from "svelte";
+
+  let stagger = 3;
+  let animation = gsap.timeline({
+    repeat: -1,
+    repeatDelay: 0,
+    defaults: { duration: 0.6, ease: "expo.inOut" },
+  });
+
+  function init() {
+    gsap.set(".rolling-text", { autoAlpha: 1 });
+    animation
+      .from(".rolling-text span", { yPercent: -100, opacity: 0, stagger })
+      .to(".rolling-text span", { yPercent: 100, opacity: 0, stagger }, stagger);
+  }
+
+  onMount(() => {
+    init();
+  });
 </script>
 
 <main class="flex flex-column pa4 mt5">
@@ -7,18 +27,18 @@
     class="flex flex-column items-center-ns items-start-l mv2-ns mv5-l"
   >
     <div class="flex flex-column items-center-ns items-start-l pb5-l pb4-ns">
-      <h2 class="f3 f2-ns f1-l fw6">
+      <h2 class="f3 f2-ns f1-l fw6 w-100">
         <span class="poppins light-red">Jacopo</span> Barone
         <br />
-        <span class="ph5-ns ph6-l">
-          Data <span class="poppins light-red">Scientist</span>
+        <span class="ph5-ns ph6-l rolling-text">
+          <span class="poppins light-red">Data Scientist</span>
+          <span class="poppins light-red">Researcher</span>
         </span>
       </h2>
       <p class="f6 f5-ns f4-m">
         My <span class="light-red"> favorite tech</span> includes JavaScript (NEXT.JS
         or SvelteKit), TailwindCSS, Node.js + Express.js & PostgreSQL or Firebase/Firestore!
       </p>
-
       <a
         class="RedShadow mv3 mr-auto-l ml0-l f6 f5-ns f4-m poppins
          relative overflow-hidden pa2 ph3 br-pill bg-white near-black hover-bg-washed-yellow
@@ -31,10 +51,17 @@
     </div>
     <div class="relative ba bw2 flex items-center justify-center">
       <img
-        src={"images/profile.jpg"}
+        src="/images/profile.jpg"
         alt="Profile"
         class="object-fit-cover z-2 h-70"
       />
+    </div>
+  </section>
+  <section id="Projects" class="flex flex-column mv2-ns mv5-l">
+    <div class="flex flex-column mv2-ns mv5-l tc">
+      <p class="f6 f4-ns f3-m poppins">
+        My <span class="poppins light-red"> Projects</span>
+      </p>
     </div>
   </section>
 </main>
