f99d09e689f2ce663f06717904e0b8bab7f5a8eb
Jacopo <baronej1990@gmail.com>
2025-03-31 16:14:01 +0100

    style: improve responsiveness of header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -14,10 +14,10 @@
       ? "pv2 bg-near-black b--near-black"
       : "pv3 bg-transparent b--transparent")}
 >
-  <h1 class="fw5">
+  <h1 class="fw5 f2-l f3-m f4-ns f5">
     <b class="fw7 poppins">Jacopo</b> Barone
   </h1>
-  <div class="flex items-center ml2">
+  <div class="flex items-center ml2 f4-l f5-m f6-ns f7">
     {#each tabs as tab, index}
       <a
         href={tab.link}
